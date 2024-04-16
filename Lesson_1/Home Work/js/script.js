4b551f3e5842e4875ada94b558e43422b4828612
Kovtun Mykhailo <kowtyn.m@gmail.com>
2024-04-16 19:03:00 +0300

    Add Home Work

--- Lesson_1/Home Work/js/script.js
+++ Lesson_1/Home Work/js/script.js
@@ -6,39 +6,56 @@
 // Виведіть значення змінної city, використовуючи функцію console.log (яка повинна показати “Іван”).
 
 // let name = "Іван";
-// let city = "Kyiv";
+// let city = "Lviv";
 // city = name;
 // console.log(city);
 
 //***2***
 //Який буде результат виконання скрипта?
+
 // let name = "Olga";
-// console.log(`привіт ${1}`); //
-// console.log(`привіт ${"name"}`); //
-// console.log(`привіт ${name}`); // ?
+// console.log(`привіт ${1}`); // привіт 1
+// console.log(`привіт ${"name"}`); // привіт name
+// console.log(`привіт ${name}`); // ? привіт Olga
 
 //***3***
 //Видобути число зі змінних
+
 // let a = "5";
 // let b = "13cvb";
 // let c = "12.9sxdcfgv";
-// вивести в консоль тип
+// // вивести в консоль тип
+// console.log(typeof Number(a), Number(a)); // виведе number 5
+// console.log(typeof parseInt(b), parseInt(b)); // виведе number 13
+// console.log(typeof parseFloat(c), parseFloat(c)); // виведе number 12.9
 
 //***4***
 //Зробіть, щоб 0.1 + 0.2 = 0.3
 
+// console.log((0.1 * 10 + 0.2 * 10) / 10);
+
 //***5**
 //Поверніть найбільше число с набору 20, 10, 50, 40
+
+// console.log(Math.max(20, 10, 50, 40));
 
 //***6**
 //Поверніть випадкове число в діапазоні від 2 до 4
 
+// console.log(Math.random() * (4 - 2) + 2);
+
 //***7**
 //дізнатись довжину message
+
 // const message = "Welcome to Bahamas!";
+// const mes = " symbols";
+// console.log(message.length + mes); // 19 symbols
 
 //***8**
 //вивести в консоль message  великими літерами
+
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase()); // WELCOME TO BAHAMAS!
 
 //***9**
 // створити пустий об*єкт
@@ -48,5 +65,27 @@
 // додати буль з ключем: like flowers
 // вивести результат в консоль
 
+// let user = {};
+// user.name = "Ivan"
+// user.age = 27;
+// user.city = "Lviv";
+// console.log(user);
+// delete user.city;
+// console.log(user);
+// user['like flowers'] = true;
+// console.log(user);
+
 //***10**
 // За допомогою циклу  “for…in” вивести в консоль ключі і значення об*єкта
+
+// let user = {};
+// user.name = "Ivan"
+// user.age = 27;
+// user.city = "Lviv";
+// delete user.city;
+// user['like flowers'] = true;
+
+// for (key in user) {
+//     console.log(key);
+//     console.log(user[key]);
+// };
