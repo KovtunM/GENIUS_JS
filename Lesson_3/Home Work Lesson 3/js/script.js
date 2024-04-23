d4c37e17ba5218d2f5009a8cfbc91cc3239d7c7f
Kovtun Mykhailo <kowtyn.m@gmail.com>
2024-04-23 12:19:00 +0300

    Add Home Work

--- Lesson_3/Home Work Lesson 3/js/script.js
+++ Lesson_3/Home Work Lesson 3/js/script.js
@@ -3,14 +3,20 @@
 // Напишіть код, який перетворює їх в масив імен.
 
 // const fruts = [
-//   { id: 0, name: "Apple" },
-//   { id: 1, name: "Tomat" },
-//   { id: 2, name: "Cherry" },
-//   { id: 3, name: "Orange" },
+//   { id: 0, name: 'Apple' },
+//   { id: 1, name: 'Tomat' },
+//   { id: 2, name: 'Cherry' },
+//   { id: 3, name: 'Orange' },
 // ];
+// let fruts_name = fruts.map(frut => frut.name);
+// console.log(fruts_name);
 
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+
+// for (let i = 2; i <= 10; i += 2) {
+//   console.log(i);
+// }
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
@@ -18,11 +24,31 @@
 //     console.lpg( `цифра ${i}!` );
 //   }
 
+// let i = 0;
+// while (i < 5) {
+//   console.log(`цифра ${i}!`);
+//   i++;
+// }
+
 //  -- 4 --
-//Напишіть цикл, який пропонує prompt ввести число більше за 100.
-//Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
-//Цикл повинен запитувати число доти, доки відвідувач не введе число,
+// Напишіть цикл, який пропонує prompt ввести число більше за 100.
+// Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
+// Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+
+// let user_input;
+// do {
+//   user_input = prompt('Будь ласка, введіть число більше за 100:');
+// } while (
+//   user_input !== null &&
+//   (isNaN(user_input) || Number(user_input) <= 100)
+// );
+
+// if (user_input !== null) {
+//   console.log('Ви ввели число', user_input);
+// } else {
+//   console.log('Ви скасували введення.');
+// }
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +59,18 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+
+// const girls = [
+//   { age: 23, name: 'Оля' },
+//   { age: 29, name: 'Аня' },
+//   { age: 10, name: 'Юля' },
+//   { age: 20, name: 'Катя' },
+// ];
+
+// let totalAge = 0;
+// for (let girl of girls) {
+//   totalAge += girl.age;
+// }
+
+// const averageAge = Math.floor(totalAge / girls.length);
+// console.log('Середній вік дівчат:', averageAge);
